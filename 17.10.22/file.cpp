47907af85185745329dea05e2b9aaf7cf51250d2
PresiianP <115057001+PresiianP@users.noreply.github.com>
2022-10-17 09:10:06 +0300

    Update file.cpp

--- 17.10.22/file.cpp
+++ 17.10.22/file.cpp
@@ -2,8 +2,9 @@
 
 int Main()
 {
-    int a,b,c;
+    int a, b, c;
     std::cin >> a >> b >> c;
-    a = a<=0?0:a; b = b<=0?0:b; c = c<=0?0:c;
-    std::cout << (a + b < c || b + c < a || c + a < b)?"е":"не е";
+    a = a <= 0 ? 0 : a; b = b <= 0 ? 0 : b; c = c <= 0 ? 0 : c;
+    bool Triangle = (a + b < c || b + c < a || c + a < b) ? 1 : 0;
+    if (Triangle) std::cout << "false"; else std::cout << "true";
 }
